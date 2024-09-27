86a334f8dea14c88e7eb0c8c6863ea3e8d8429eb
MizaHilmiya <mizahilmiya02@gmail.com>
2024-09-27 10:16:46 +0700

    Add Inheritance.kt

--- src/main/kotlin/id/ac/polbeng/miza/test_kelas/InheritanceB.kt
+++ src/main/kotlin/id/ac/polbeng/miza/test_kelas/InheritanceB.kt
@@ -1,17 +1,13 @@
 package id.ac.polbeng.miza.test_kelas
-
 //chile class (initializes the parent class)
 class LaptopB : Computer {
     val batteryLife: Double
-
     //Calls super() to initialize the Parent class
     constructor(name: String, brand: String, batteryLife: Double) : super(name, brand) {
         this.batteryLife = batteryLife
     }
-
     //calls another constructor(which calls super())
     constructor(name: String, brand: String) : this(name, brand, 0.0) {
-
     }
     fun info(){
         println("Name : $name")
@@ -23,4 +19,3 @@ fun main(){
     val myLaptop = LaptopB("Asus Think Pad", "Asus")
     println(myLaptop.info())
 }
-
